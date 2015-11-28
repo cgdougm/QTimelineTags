0dbe4774ade1c4e249a21adc06ea28b8f3bca6f5
Doug MacMillan <github@raette.com>
2015-11-28 12:03:42 -0800

    pep-8

--- qtimeline.py
+++ qtimeline.py
@@ -1,7 +1,7 @@
 
-__author__ = 'cgdougm'
-
 from PyQt4 import QtCore, QtGui
+
+__author__ = 'cgdougm'
 
 
 ##########################################
@@ -20,61 +20,62 @@ class FrameTimeline(QtGui.QGraphicsScene):
         #
         self.setBackgroundBrush(QtCore.Qt.lightGray)
         #
-        #l = QtGui.QGraphicsAnchorLayout()
-        #l.setSpacing(0)
-        #self.setLayout(l)
+        # l = QtGui.QGraphicsAnchorLayout()
+        # l.setSpacing(0)
+        # self.setLayout(l)
         #
-        self._framebarRectItem = None
-        self._framebarTextItems = list()
-
+        self._frame_bar_rect_item = None
+        self._frame_bar_text_items = list()
 
     @property
-    def timeRange(self):
+    def time_range(self):
         return self.endTime - self.startTime
 
     def build(self):
-        self.buildFrameBar()
+        self.build_frame_bar()
         if self.currentTime is not None:
-            self.buildCurrentTime()
-        self.updatePositions()
+            self.build_current_time()
+        self.update_positions()
 
-    def buildFrameBar(self):
-        self._framebarTextItems = list()
-        r = self.addRect(0, 0, self.timeRange, self.height,
-                     QtGui.QPen(QtCore.Qt.transparent),
-                     QtGui.QBrush(QtCore.Qt.darkGray))
+    def build_frame_bar(self):
+        self._frame_bar_text_items = list()
+        r = self.addRect(0,
+                         0,
+                         self.time_range,
+                         self.height,
+                         QtGui.QPen(QtCore.Qt.transparent),
+                         QtGui.QBrush(QtCore.Qt.darkGray))
         drop = QtGui.QGraphicsDropShadowEffect()
         drop.setBlurRadius(6.0)
         drop.setOffset(4.0, 4.0)
         r.setGraphicsEffect(drop)
-        self._framebarRectItem = r
+        self._frame_bar_rect_item = r
         for t in range(self.startTime, self.endTime, self.timeSpace):
             textItem = self.addText(str(t))
             textItem.setPos(t * self.unitsPerTime, 0.0)
-            self._framebarTextItems.append(textItem)
+            self._frame_bar_text_items.append(textItem)
 
-    def buildCurrentTime(self):
+    def build_current_time(self):
         tx = self.currentTime * self.unitsPerTime
-        currBg = self.addRect(-2, -2, 30, 30, brush=QtGui.QBrush(QtCore.Qt.black))
-        currBg.setOpacity(0.2)
-        currBg.setPos(tx, 0.0)
-        currTextItem = self.addText(str(self.currentTime))
-        currTextItem.setPos(tx, 0.0)
-        boldFont = currTextItem.font()
-        boldFont.setBold(True)
-        currTextItem.setFont(boldFont)
+        current_background = self.addRect(-2, -2, 30, 30, QtGui.QBrush(QtCore.Qt.black))
+        current_background.setOpacity(0.2)
+        current_background.setPos(tx, 0.0)
+        current_text_item = self.addText(str(self.currentTime))
+        current_text_item.setPos(tx, 0.0)
+        bold_font = current_text_item.font()
+        bold_font.setBold(True)
+        current_text_item.setFont(bold_font)
 
-    def updatePositions(self):
+    def update_positions(self):
         # Rectangle
-        transf = self._framebarRectItem.transform()
-        transf.reset()
-        transf.scale(self.unitsPerTime, 1.0)
-        self._framebarRectItem.setTransform(transf)
+        transform = self._frame_bar_rect_item.transform()
+        transform.reset()
+        transform.scale(self.unitsPerTime, 1.0)
+        self._frame_bar_rect_item.setTransform(transform)
         # Text
         for i, t in enumerate(range(self.startTime, self.endTime, self.timeSpace)):
-            text = self._framebarTextItems[i]
+            text = self._frame_bar_text_items[i]
             text.setPos(t * self.unitsPerTime, 0.0)
-
 
 
 ##########################################
@@ -95,7 +96,7 @@ if __name__ == '__main__':
         def wheelEvent(self, event):
             delta = float(event.delta()) / 120.0
             g.unitsPerTime += delta
-            g.updatePositions()
+            g.update_positions()
 
     w = DemoView()
     w.setScene(g)
